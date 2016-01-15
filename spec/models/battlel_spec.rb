23a08121251dd3c447a321667e82b3c1778142cd
jamarin6 <jamarinhidalgo@gmail.com>
2016-01-15 09:04:55 +0100

    spec models added for testing

---
+++ spec/models/battlel_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+# require 'spec_helper'
+
+RSpec.describe Battlel, type: :model do
+  # describe "validation" do
+  #   it "is valid with valid data" do
+  #     @battlel = Battlel.create!(pet1: "chuli", pet2: "dixie", date: "2016-01-15 09:02:27 +0100", winner: "chuli")
+  #     expect(@order).to be_valid
+  #   end
+  # end
+end
