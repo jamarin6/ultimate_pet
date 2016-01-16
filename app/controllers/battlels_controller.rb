dbaca5c706597b76de3b6ff6032597dfa42117e1
jamarin6 <jamarinhidalgo@gmail.com>
2016-01-16 09:28:41 +0100

    added restrictions for battlels

--- app/controllers/battlels_controller.rb
+++ app/controllers/battlels_controller.rb
@@ -24,27 +24,13 @@ class BattlelsController < ApplicationController
 
   # POST /battlels
   # POST /battlels.json
-  # def create
-  #   @battlel = Battlel.new(battlel_params)
-
-  #   respond_to do |format|
-  #     if @battlel.save
-  #       format.html { redirect_to @battlel, notice: 'Battlel was successfully created.' }
-  #       format.json { render :show, status: :created, location: @battlel }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @battlel.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   def create
     my_pet1 = Pet.find_by(name: battlel_params[:pet1])
     my_pet2 = Pet.find_by(name: battlel_params[:pet2])
 
-    if my_pet1.user_id==my_pet2.user_id
+    if (my_pet1.user_id==my_pet2.user_id) ||  (my_pet1.is_in_battlel_today?) || (my_pet2.is_in_battlel_today?)
       respond_to do |format|
-        format.html { redirect_to battlels_path, notice: 'Battlel was not created (same owner is not possible).' }
+        format.html { redirect_to battlels_path, notice: 'Battlel was not created (same owner is not possible & can not be a pet in two battles on the same day).' }
       end
     else
       @battlel = Battlel.new(battlel_params)
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,11 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
+
+  def is_in_battlel_today?
+    if Battlel.where(pet1: name, created_at: Date.today.beginning_of_day..Date.today.end_of_day).any? || Battlel.where(pet2: name, created_at: Date.today.beginning_of_day..Date.today.end_of_day).any?
+      return true
+    else
+      return false
+    end
+  end
 end
