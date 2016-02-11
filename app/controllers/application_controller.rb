5af97076e07b2faf3b2c35acd6fed0b16586730d
jamarin6 <jamarinhidalgo@gmail.com>
2016-02-11 18:09:14 +0100

    adding i18n example (translate)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  before_filter :set_locale
   protect_from_forgery with: :exception
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def default_url_options(options={})
+    { locale: I18n.locale == :en ? '' : I18n.locale }
+  end
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym) 
+      I18n.locale = params[:locale]
+    else
+      I18n.locale = :en
+    end
+  end
 
   def after_sign_in_path_for(user)
     pets_path
@@ -15,6 +28,10 @@ class ApplicationController < ActionController::Base
     def user_not_authorized
       flash[:alert] = "You are not authorized to perform this action."
       redirect_to(request.referrer || root_path)
+    end
+
+    def extract_locale_from_accept_language_header
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,24 @@ Bundler.require(*Rails.groups)
 
 module UltimatePet
   class Application < Rails::Application
+            # Check and load application configuration file
+            # config.before_configuration do
+            #   ::APP_CONFIG = ::ActiveSupport::OrderedOptions.new
+            #   config_yml = File.join(::Rails.root, 'config' , 'application.yml')
+            #   if not File.exists?(config_yml)
+            #     puts "Missing config/application.yml, aborting"
+            #    exit 1
+            #   end
+            #   YAML.load_file(config_yml)[::Rails.env].each do |key, value|
+            #     ::APP_CONFIG[key] = value
+            #   end
+            # end
+
+            # # Custom directories with classes and modules you want to be autoloadable.
+            # config.autoload_paths << Rails.root.join('lib')
+            # # Configure the default encoding used in templates for Ruby 1.9.
+            # config.encoding = "utf-8"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -17,10 +35,12 @@ module UltimatePet
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.i18n.available_locales = [:es, :en]
   end
 end
